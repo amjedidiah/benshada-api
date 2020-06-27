4b8fd8b011f4bfb372b5371c55db41f2a593a219
amjedidiah <imunacode@gmail.com>
2020-06-28 00:33:15 +0100

    feat: Updated Products model for sizes && isBatch

--- models/Products.js
+++ models/Products.js
@@ -1,85 +1,101 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
-const ProductsSchema = new Schema({
-	name: {
-		required: true,
-		type: String
-	},
-	shortDescription: {
-		type: String,
-		required: true
-	},
-	longDescription: {
-		type: String,
-		required: true
-	},
-	specifications: {
-		size: String,
-		color: String,
-		mainMaterial: String,
-		weight: String,
-		productionCountry: String
-	},
-	guarantee: {
-		type: Number,
-		required: true
-	},
-	shop: {
-		ref: 'Shops',
-		type: Schema.Types.ObjectId
-	},
-	reviews: [{
-		ref: 'Reviews',
-		type: Schema.Types.ObjectId
-	}],
-	category: {
-		type: String,
-		required: false
-	},
-	gender: {
-		type: String,
-		required: true,
-		enum: ['male', 'female']
-	},
-	quantity: {
-		type: Number,
-		default: 0
-	},
-	image: [{
-		type: String,
-	}],
-	overallRating: {
-		type: Number,
-		default: 0
-	},
-	inStock: {
-		type: Boolean,
-		default: true
-	},
-	price: {
-		type: Number,
-		required: true
-	},
-	returns: {
-		type: Number,
-		default: 0,
-	},
-	discountPercentage: {
-		type: Number,
-		default: 0
+const ProductsSchema = new Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    shortDescription: {
+      type: String,
+      required: true,
+    },
+    longDescription: {
+      type: String,
+      required: true,
+    },
+    specifications: {
+      size: String,
+      color: String,
+      mainMaterial: String,
+      weight: String,
+      productionCountry: String,
+    },
+    guarantee: {
+      type: Number,
+      required: true,
+    },
+    shop: {
+      ref: "Shops",
+      type: Schema.Types.ObjectId,
+    },
+    reviews: [
+      {
+        ref: "Reviews",
+        type: Schema.Types.ObjectId,
+      },
+    ],
+    category: {
+      type: String,
+      required: false,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+    image: [
+      {
+        type: String,
+      },
+    ],
+    sizes: [
+      {
+        type: String,
+      },
+    ],
+    overallRating: {
+      type: Number,
+      default: 0,
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    returns: {
+      type: Number,
+      default: 0,
+    },
+    discountPercentage: {
+      type: Number,
+      default: 0,
+    },
+    isBlocked: {
+      default: false,
+      type: Boolean,
+    },
+    isDeleted: {
+      default: false,
+      type: Boolean,
+    },
+    isBatch: {
+      default: false,
+      type: Boolean,
+    },
   },
-  isBlocked: {
-    default: false,
-    type: Boolean
-  },
-	isDeleted: {
-		default: false,
-		type: Boolean
-	},
-}, {
-	timestamps: true
-})
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Products', ProductsSchema)
+module.exports = mongoose.model("Products", ProductsSchema);
